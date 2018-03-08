5ca025aa633e8f1c03cbb178ce5aaa8c1eea941a
Cordelia McGee-Tubb <c.mcgeetubb@salesforce.com>
2018-03-07 17:35:01 -0800

    remove commented out announcements, as they're no longer needed

--- src/components/Listbox/Listbox.jsx
+++ src/components/Listbox/Listbox.jsx
@@ -114,14 +114,6 @@ class Listbox extends Component {
         startIndex = this.state.selectedOptions[0];
         var newOption = this.findNewOption(currentOption, moveNext);
         this.handleDragStateChange(startIndex, newOption);
-
-        /* 
-         Note(CMT): Removing this announcement for now as I suspect AT does this work for us.
-
-         grabbedOptionName = this.state.listOptions[startIndex].props.name;
-         ariaLiveText = this.updateAssistiveText(grabbedOptionName, newOption, false, true);
-         this.setState({ariaLiveText});
-        */
       } else {
         this.handleSingleSelectKeyDown(event);
       }
